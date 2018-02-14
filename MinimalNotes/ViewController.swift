e53354eb9cbb9d32f2b9cebb689255930aa8b172
Leandro Pessini <leandro@pessini.me>
2018-02-14 10:02:33 +0100

    Initial Commit

---
+++ MinimalNotes/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MinimalNotes
+//
+//  Created by Leandro Pessini on 14/02/2018.
+//  Copyright © 2018 Leandro Pessini. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
